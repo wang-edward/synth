50b373ffd84ec950bb41e0ccbc6f498bb21265a1
Edward Wang <edawordwang@gmail.com>
2025-10-11 03:27:21 -0400

    update build.zig for 0.15.1

--- build.zig
+++ build.zig
@@ -7,20 +7,22 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = std.builtin.OptimizeMode.Debug;
 
-    const lib = b.addStaticLibrary(.{
-        .name = "synth",
+    const mod = b.addModule("synth", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(lib);
-
     const exe = b.addExecutable(.{
         .name = "synth",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "synth", .module = mod },
+            },
+        }),
     });
     exe.linkSystemLibrary("soundio");
     exe.linkLibC();
@@ -35,21 +37,18 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
+    const mod_tests = b.addTest(.{
+        .root_module = mod,
     });
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = exe.root_module,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
