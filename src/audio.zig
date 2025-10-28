68a55334c66a7ec3dae881677c9454c829ad9497
Edward Wang <edawordwang@gmail.com>
2025-10-28 04:33:22 -0400

    Osc (#4)
    * osc struct
    
    * fix errors
    
    * fix pwm and sub
    
    * use them in main
    
    * delete Sine
    
    ---------
    
    Co-authored-by: Edward Wang <edw@Edwards-Mac-mini.local>

--- src/audio.zig
+++ src/audio.zig
@@ -21,25 +21,42 @@ pub const Context = struct {
     }
 };
 
-pub const Sine = struct {
+pub const Osc = struct {
     freq: f32,
-    phase: f32, // 0..1 cycles
-    vt: VTable = .{ .process = Sine._process },
+    phase: f32,
+    kind: Kind,
+    vt: VTable = .{ .process = Osc._process },
 
-    pub fn init(freq: f32) Sine {
-        return .{ .freq = freq, .phase = 0 };
+    pub const Kind = union(enum) {
+        sine: struct {},
+        pwm: struct { duty: f32 = 0.5 },
+        saw: struct {},
+        sub: struct { duty: f32 = 0.5, offset: f32 = -12 },
+    };
+
+    pub fn init(freq: f32, kind: Kind) Osc {
+        return .{ .freq = freq, .phase = 0, .kind = kind };
     }
     fn _process(p: *anyopaque, ctx: *Context, out: []Sample) void {
-        var self: *Sine = @ptrCast(@alignCast(p));
-        const inc = self.freq / ctx.sample_rate;
-        var i: usize = 0;
-        while (i < out.len) : (i += 1) {
-            out[i] = @floatCast(std.math.sin(self.phase * 2.0 * std.math.pi));
+        var self: *Osc = @ptrCast(@alignCast(p));
+        const base_inc = self.freq / ctx.sample_rate;
+        const inc = switch (self.kind) {
+            .sub => |sub| base_inc * std.math.exp2(sub.offset / 12.0),
+            else => self.freq / ctx.sample_rate,
+        };
+        for (0..out.len) |i| {
+            const sample: Sample = switch (self.kind) {
+                .sine => std.math.sin(self.phase * 2.0 * std.math.pi),
+                .pwm => |pwm| if (self.phase < pwm.duty) 1.0 else -1.0,
+                .saw => 2.0 * self.phase - 1.0,
+                .sub => |sub| if (self.phase < sub.duty) 1.0 else -1.0,
+            };
+            out[i] = @floatCast(sample);
             self.phase += inc;
             if (self.phase >= 1.0) self.phase -= 1.0;
         }
     }
-    pub fn asNode(self: *Sine) Node {
+    pub fn asNode(self: *Osc) Node {
         return .{ .ptr = self, .v = &self.vt };
     }
 };
