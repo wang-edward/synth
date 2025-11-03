50c3b5b94ac5339b67c7f7513a8036d1f0632f3e
Edward Wang <edawordwang@gmail.com>
2025-11-03 01:33:00 -0500

    WIP adding mixers, asNode() and (tbd) process() to voice and synth

--- src/synth.zig
+++ src/synth.zig
@@ -11,37 +11,63 @@ const Voice = struct {
     saw: audio.Osc,
     sub: audio.Osc,
     // noise: audio.Noise, // TODO
+    mix: *audio.Mixer, // TODO rename mixer
 
     noteState: NoteState = .Off,
 
-    pub fn init(freq: f32) Voice {
+    pub fn init(alloc: std.mem.Allocator, freq: f32) !Voice {
+        var pwm = audio.Osc.init(freq, .{ .pwm = .{} });
+        var saw = audio.Osc.init(freq, .{ .saw = .{} });
+        var sub = audio.Osc.init(freq, .{ .sub = .{} });
+        const mixer = try audio.Mixer.init(alloc, &[_]*const audio.Node{
+            &pwm.asNode(), &saw.asNode(), &sub.asNode(),
+        });
         return .{
-            .pwm = audio.Osc.init(freq, .{ .pwm = .{} }),
-            .saw = audio.Osc.init(freq, .{ .saw = .{} }),
-            .sub = audio.Osc.init(freq, .{ .sub = .{} }),
+            .pwm = pwm,
+            .saw = saw,
+            .sub = sub,
+            .mix = mixer,
             .noteState = .Off,
         };
+    }
+    pub fn deinit(self: *Voice, alloc: std.mem.Allocator) void {
+        alloc.free(self.mix.inputs);
+        alloc.destroy(self.mix);
+    }
+    pub fn asNode(self: *Voice) *const audio.Node {
+        return self.mix.asNode();
     }
 };
 
 const Synth = struct {
     const SYNTH_TUNING: f32 = 440.0;
     voices: []Voice,
+    mixer: *audio.Mixer,
     next_idx: usize = 0,
 
-    pub fn init(allocator: std.mem.Allocator, count: usize) !*Synth {
-        const s = try allocator.create(Synth);
-        s.voices = try allocator.alloc(Voice, count);
-        s.next_idx = 0;
+    pub fn init(alloc: std.mem.Allocator, count: usize) !*Synth {
+        const s = try alloc.create(Synth);
+        s.voices = try alloc.alloc(Voice, count);
         for (s.voices) |*v| v.* = Voice.init(0.0);
+
+        const NODE_BUF_SIZE = 16;
+        std.debug.assert(count <= NODE_BUF_SIZE);
+        var node_buf: [NODE_BUF_SIZE]*const audio.Node = undefined;
+        const nodes = node_buf[0..count];
+        for (s.voices, 0..) |*v, i| nodes[i] = v.asNode(); // const?
+
+        s.mixer = try audio.Mixer.init(alloc, nodes);
+        s.next_idx = 0;
+
         return s;
     }
-
-    pub fn deinit(self: *Synth, allocator: std.mem.Allocator) void {
-        allocator.free(self.voices);
-        allocator.destroy(self);
+    pub fn deinit(self: *Synth, alloc: std.mem.Allocator) void {
+        for (self.voices) |*v| v.deinit(alloc);
+        alloc.free(self.mix.inputs);
+        alloc.destroy(self.mix);
+        alloc.free(self.voices);
+        alloc.destroy(self);
     }
-
     pub fn noteOn(self: *Synth, note: u8) void {
         const idx = self.next_idx;
         self.next_idx = (self.next_idx + 1) % self.voices.len;
@@ -53,7 +79,6 @@ const Synth = struct {
         v.sub.freq = freq;
         v.noteState = NoteState.On(note);
     }
-
     pub fn noteOff(self: *Synth, note: u8) void {
         for (self.voices) |*v| {
             switch (v.noteState) {
@@ -65,6 +90,9 @@ const Synth = struct {
             }
         }
         // TODO raise warning if note not found?
+    }
+    pub fn asNode(self: *Voice) *const audio.Node {
+        return self.mix.asNode();
     }
 };
 
