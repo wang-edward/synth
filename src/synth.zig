4a149fc97f02b0d7f05195075265640e5f6607e8
Edward Wang <edawordwang@gmail.com>
2025-11-19 04:18:47 -0500

    Synth enum is not actually necessary cuz lesynth is not owned by sequencer

--- src/synth.zig
+++ src/synth.zig
@@ -1,29 +1,9 @@
 const audio = @import("audio.zig");
-const uni = @import("uni.zig");
 const SpscQueue = @import("queue.zig").SpscQueue;
 
 pub const NoteMsg = union(enum) {
     On: u8,
     Off: u8,
-};
-
-pub const Synth = union(enum) {
-    Uni: uni.Uni,
-    pub fn noteOn(self: *@This(), note: u8) void {
-        switch (self.*) {
-            .Uni => |*u| u.noteOn(note),
-        }
-    }
-    pub fn noteOff(self: *@This(), note: u8) void {
-        switch (self.*) {
-            .Uni => |*u| u.noteOff(note),
-        }
-    }
-    pub fn process(self: *@This(), ctx: *audio.Context, out: []f32) void {
-        switch (self.*) {
-            .Uni => |*u| u.process(ctx, out),
-        }
-    }
 };
 
 pub const NoteQueue = SpscQueue(NoteMsg, 16);
