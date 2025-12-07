e237c170906fd765fe5ffd1ccd97bd346c14ae30
Edward Wang <edawordwang@gmail.com>
2025-12-07 14:18:34 -0500

    update build for graphics_demo

--- build.zig
+++ build.zig
@@ -7,15 +7,16 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = std.builtin.OptimizeMode.Debug;
 
+    // raylib
     const raylib_dep = b.dependency("raylib_zig", .{
         .target = target,
         .optimize = optimize,
     });
-
     const raylib = raylib_dep.module("raylib"); // main raylib module
     const raygui = raylib_dep.module("raygui"); // raygui module
     const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
 
+    // soundio
     const soundio_dep = b.dependency("libsoundio", .{
         .target = target,
         .optimize = optimize,
@@ -23,6 +24,7 @@ pub fn build(b: *std.Build) void {
     const soundio_mod = soundio_dep.module("SoundIo");
     const soundio_artifact = soundio_dep.artifact("soundio");
 
+    // exe
     const exe = b.addExecutable(.{
         .name = "synth",
         .root_module = b.createModule(.{
@@ -40,22 +42,38 @@ pub fn build(b: *std.Build) void {
     exe.linkLibrary(soundio_artifact);
     exe.linkLibC();
     b.installArtifact(exe);
-
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    if (b.args) |args| run_cmd.addArgs(args);
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
+    // graphics
+    const graphics_demo = b.addExecutable(.{
+        .name = "graphics",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("examples/graphics_demo.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "raylib", .module = raylib },
+            },
+        }),
+    });
+    graphics_demo.linkLibrary(raylib_artifact);
+    graphics_demo.linkLibC();
+    b.installArtifact(graphics_demo);
+    const run_cmd_demo = b.addRunArtifact(graphics_demo);
+    if (b.args) |args| run_cmd_demo.addArgs(args);
+    const run_step_demo = b.step("demo", "Run emulated graphics");
+    run_step_demo.dependOn(&run_cmd_demo.step);
+
+    // test
     const queue_mod = b.createModule(.{
         .root_source_file = b.path("src/queue.zig"),
         .target = target,
         .optimize = optimize,
     });
-
     const unit_tests = b.addTest(.{
         .root_module = b.createModule(.{
             .root_source_file = b.path("test/test_queue.zig"),
@@ -66,9 +84,7 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
         }),
     });
-
     const run_exe_unit_tests = b.addRunArtifact(unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 }
