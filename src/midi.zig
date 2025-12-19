d5c9af13468f8898e37723d442e6dc1d31d495aa
Edward Wang <edawordwang@gmail.com>
2025-12-18 20:35:12 -0500

    remove Midi prefix within midi.zig

--- src/midi.zig
+++ src/midi.zig
@@ -4,7 +4,7 @@ const synth = @import("synth.zig");
 
 pub const Frame = u64;
 
-pub const MidiNote = struct {
+pub const Note = struct {
     start: Frame,
     end: Frame,
     note: u8,
@@ -14,22 +14,22 @@ pub fn beatsToSamples(beats: u64, tempo: f32, ctx: *audio.Context) Frame {
     return tempo * 60 * ctx.sample_rate * beats;
 }
 
-pub const MidiPlayer = struct {
-    notes: []MidiNote,
+pub const Player = struct {
+    notes: []Note,
     sample_accum: Frame = 0,
-    pub fn init(alloc: std.mem.Allocator, notes_in: []const MidiNote) !MidiPlayer {
-        const notes = try alloc.alloc(MidiNote, notes_in.len);
-        std.mem.copyForwards(MidiNote, notes, notes_in);
+    pub fn init(alloc: std.mem.Allocator, notes_in: []const Note) !Player {
+        const notes = try alloc.alloc(Note, notes_in.len);
+        std.mem.copyForwards(Note, notes, notes_in);
 
         return .{
             .notes = notes,
             .sample_accum = 0,
         };
     }
-    pub fn deinit(self: *MidiPlayer, alloc: std.mem.Allocator) void {
+    pub fn deinit(self: *Player, alloc: std.mem.Allocator) void {
         alloc.free(self.notes);
     }
-    pub fn advance(self: *MidiPlayer, samples_elapsed: u64, q: *synth.NoteQueue) void {
+    pub fn advance(self: *Player, samples_elapsed: u64, q: *synth.NoteQueue) void {
         const pre_accum = self.sample_accum;
         const post_accum = self.sample_accum + samples_elapsed;
         self.sample_accum = post_accum;
