b86eff1bfaa008a5939b5094833b735d65d8a2e8
Edward Wang <edawordwang@gmail.com>
2025-12-26 01:36:54 -0500

    ops

--- src/ops.zig
+++ src/ops.zig
@@ -5,8 +5,26 @@ pub const PlaybackOp = union(enum) {
     Seek: u64,
 };
 
+pub const ParamId = union(enum) {
+    Lpf: LpfParam,
+};
+
+pub const LpfParam = enum {
+    Drive,
+    Resonance,
+    Cutoff,
+};
+
+pub const ParamOp = struct {
+    track_id: usize,
+    plugin_id: usize,
+    id: ParamId,
+    value: f32,
+};
+
 pub const Op = union(enum) {
     Playback: PlaybackOp,
+    Param: ParamOp,
 };
 
 pub const OpQueue = SpscQueue(Op, 32);
